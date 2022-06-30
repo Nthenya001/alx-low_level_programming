30218c96d71f1b8bcf7c82b8eeba760f09d5df7d
Nthenya <faithjack001@gmail.com>
2022-06-30 13:26:48 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
 * Prints a line to the commandline using printf
 * Return: Always 0 (Success)
 */
-int main(void)
-{
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+int main(void){
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19 \n");
 	return (1);
 }
