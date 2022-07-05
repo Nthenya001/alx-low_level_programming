15ebac33a9f36986616dd21124ace7df0e2084d1
Nthenya <faithjack001@gmail.com>
2022-07-05 12:26:19 -0700

    Write a program that prints _putchar, followed by a new line.
    The program should return 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,7 @@
  *
  * Return: nothing, return void
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('-');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return(0);
+	return(write(1, &c, 1));
 }
