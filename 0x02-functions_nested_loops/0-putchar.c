02690cb885af1154f628a682de5527067b60e39a
Nthenya <faithjack001@gmail.com>
2022-07-05 12:55:58 -0700

    putchar printed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,17 @@
  *
  * Return: nothing, return void
  */
-int _putchar(char c)
+int main(void)
 {
-	return(write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return(0);
 }
