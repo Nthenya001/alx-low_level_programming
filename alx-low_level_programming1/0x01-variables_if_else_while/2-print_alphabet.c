b8b1abab2a1e67de913548607d17edb2d3e2dc54
Nthenya <faithjack001@gmail.com>
2022-08-17 06:08:49 -0700

    commit

---
+++ alx-low_level_programming1/0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always returns 0 (success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ alx-low_level_programming1/0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strstr - first occurrence of the substring needle in the string haystack
+ * @haystack: main str to be examined
+ * @needle: searched in haystack
+ * Return: return 0
+ **/
+char  *_strstr(char *haystack, char *needle)
+{
+	char *str1, *str2; /*Declaring variables*/
+
+	while (*haystack != '\0')
+	{
+		str1 = haystack; /*values*/
+		str2 = needle;
+
+
